b83c99876355479bec183242a9b9916e64a74a7e
pranavnaikp <pranavnaik250901@gmail.com>
2023-07-02 13:13:15 +0530

    updated navbar

---
+++ src/components/SideNav/SlidebarData.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as MdIcons from "react-icons/md";
+import * as RiIcons from "react-icons/ri";
+
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text"
+  },
+
+  {
+    title: "Your Account",
+    path: "/account",
+    icon: <MdIcons.MdAccountCircle/>,
+    cName: "nav-text"
+  },
+
+  {
+    title: "Physical health score",
+    path: "/Wishlisr",
+    icon: <MdIcons.MdHealthAndSafety />,
+    cName: "nav-text"
+  },
+
+  {
+    title: "Mental Health Score",
+    path: "/",
+    icon: <RiIcons.RiMentalHealthFill />,
+    cName: "nav-text"
+  },
+
+  {
+    title: "Reports",
+    path: "/reports",
+    icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text"
+  },
+
+  
+  {
+    title: "Support",
+    path: "/support",
+    icon: <IoIcons.IoMdHelpCircle />,
+    cName: "nav-text"
+  }
+];
--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,5 +1,58 @@
-export default function HomePage() {
-    return (
-        "HomePage"
-    )
-}
+import React, { useState } from 'react';
+import './styles.module.css';
+
+const HomePage = () => {
+  const [empId, setEmpId] = useState('');
+  const [empPassword, setEmpPassword] = useState('');
+  const [mngId, setMngId] = useState('');
+  const [mngPassword, setMngPassword] = useState('');
+
+  const handleEmpLogin = () => {
+    // Handle employee login logic here
+    console.log('Employee Login:', empId, empPassword);
+  };
+
+  const handleMngLogin = () => {
+    // Handle manager login logic here
+    console.log('Manager Login:', mngId, mngPassword);
+  };
+
+  return (
+    <div className="homepage-container">
+      <div className="login-section">
+        <h2>Employee Login</h2>
+        <input
+          type="text"
+          placeholder="Employee ID"
+          value={empId}
+          onChange={(e) => setEmpId(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={empPassword}
+          onChange={(e) => setEmpPassword(e.target.value)}
+        />
+        <button onClick={handleEmpLogin}>Login</button>
+      </div>
+      <div className="login-section">
+        <h2>Manager Login</h2>
+        <input
+          type="text"
+          placeholder="Manager ID"
+          value={mngId}
+          onChange={(e) => setMngId(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={mngPassword}
+          onChange={(e) => setMngPassword(e.target.value)}
+        />
+        <button onClick={handleMngLogin}>Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
